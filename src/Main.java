47b92279fa6aee8c8adf16cb9650a6aee3607e55
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-05-09 15:17:03 +0300

    add Iterator method

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        LinkedHashSet<Integer> lhs = new LinkedHashSet<Integer>();
+
+        lhs.add(2);
+        lhs.add(4);
+        lhs.add(3);
+        lhs.add(5);
+
+//        System.out.println("lhs content is " +lhs);
+
+        Iterator<Integer> it = lhs.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + ",");
+
         }
+
     }
 }
